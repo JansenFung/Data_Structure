80ed663a88418556002a3fc35287f51730c12054
Jansen Fung <54913752+JansenFung@users.noreply.github.com>
2022-03-15 14:06:47 -0400

    added randomArray

--- sorting.js
+++ sorting.js
@@ -1,5 +1,16 @@
 class Sorting{
     static randomElement(max, min){
-        return  Math.floor(Math.random() * (max - min + 1) + min)
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    static randomArray(size){
+        if(size <= 0){
+            return []
+        }  
+        else{
+            const array = Sorting.randomArray(size - 1)
+            array.push(Sorting.randomElement(20, -10))
+            return array
+        }
     }
 }
