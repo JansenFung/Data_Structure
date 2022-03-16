10c30ce56539fb35981ff974754c0321be751d47
Jansen Fung <54913752+JansenFung@users.noreply.github.com>
2022-03-16 14:29:54 -0400

    added randomArray

--- select.js
+++ select.js
@@ -2,4 +2,15 @@ class Select{
     static randomElement(max, min){
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
+
+    static randomArray(size){
+        if(size <= 0){
+            return [];
+        }
+        else{
+            const array = Select.randomArray(size - 1);
+            array.push(Select.randomElement(20, -10));
+            return array;
+        }
+    }
 }
