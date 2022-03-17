772fe4643b23eed817706fc332bd6a4d1fcff050
Jansen Fung <54913752+JansenFung@users.noreply.github.com>
2022-03-17 16:10:16 -0400

    binarySearch

--- search.js
+++ search.js
@@ -1,3 +1,5 @@
+import {Sorting} from './sorting.js';
+
 class Search{
     static randomElement(max, min){
         return Math.floor(Math.random() * (max - min + 1) + min);
@@ -22,4 +24,39 @@ class Search{
 
         return -1;
     }
+
+    //while loop
+    static binarySearch(array, target, startIndex = 0, endingIndex = array.length - 1){
+        while(startIndex <= endingIndex){
+            let middle = Math.floor((startIndex + endingIndex) / 2);
+
+            if(array[middle] === target)
+                return middle;
+            else if (target < array[middle])
+                endingIndex = middle - 1;
+            else
+                startIndex = middle + 1;
+        }
+
+        return -1;
+    }
+
+    //Recursive approach
+    static recursiveBinarySearch(array, target, startIndex = 0, endingIndex = array.length - 1){
+        if (startIndex > endingIndex)
+            return -1;
+        
+        let middle = Math.floor((startIndex + endingIndex) / 2);
+
+        if(array[middle] === target)
+            return middle;
+        else if (target < array[middle])
+            return Search.recursiveBinarySearch(array, target, startIndex, middle - 1);
+        else
+            return Search.recursiveBinarySearch(array, target, middle + 1, endingIndex);
+    }
 }
+
+let x = Search.randomArray(10);
+
+console.log(Sorting.quickSort(x), Search.recursiveBinarySearch(x, 6))
